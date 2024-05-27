e5673128cc016feb9a2953505bdea43f8d1ed33f
Aldous Angelo S. Dignos <aldous.dignos@technomancer.biz>
2024-05-27 14:31:36 +0800

    remove commented lines

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,22 +1,4 @@
-{{-- <!DOCTYPE html>
-<html>
-<head>
-    <title>Laravel CRUD</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-</head>
-<body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="{{ url('/') }}">Laravel CRUD</a>
-    </nav>
-
-    @include('users.alert')
-
-    <div class="container mt-4">
-        @yield('content')
-    </div>
-</body>
-</html> --}}
-
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
